a98ecd447e64e376c38cd69261eb65ab239aff4d
fzx <648215109@qq.com>
2019-04-03 17:49:27 +0800

    thread Local

---
+++ test/test1.py
@@ -0,0 +1,27 @@
+
+
+
+
+# 线程隔离werkzeug local Local 字典
+
+
+# L 线程隔离对象
+import threading
+import time
+from werkzeug.local import Local
+
+
+class A:
+    b = 1
+
+m = Local()
+m.b = 1
+def worker():
+    m.b =100
+    print('new',m.b)
+
+new = threading.Thread(target=worker,name='qq')
+new.start()
+time.sleep(1)
+
+print(m.b)
