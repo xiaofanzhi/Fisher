7ee6c992f725a42e6fb2c23f3771cf27d7d1bed9
fzx <648215109@qq.com>
2019-05-04 12:37:44 +0800

    Update fisher.py

--- fisher.py
+++ fisher.py
@@ -5,6 +5,6 @@ from app import create_app
 app = create_app()
 
 if __name__=='__main__':
-    app.run(debug=app.config['DEBUG'],port='9999',threaded = True,)
+    app.run(debug=app.config['DEBUG'],port='9999')
 #     processes = 1 多线程
 
